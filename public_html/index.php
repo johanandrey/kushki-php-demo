90d31afc16bd333fea8b0d0455bf4cec87930046
johanandrey <johanandrey.baron@gmail.com>
2017-12-19 18:13:31 -0500

    variables de ambiente 3

--- public_html/index.php
+++ public_html/index.php
@@ -131,8 +131,8 @@ session_start();
         <p><br /><br /><br /></p>
        
         <!-- scripts and more -->
-        <script src="kushkijs"></script>
-        <script src="kushkicheckoutjs"></script>
+        <script src="KUSHKIJS"></script>
+        <script src="KUSHKICHECKOUTJS"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
         <script data-prerender="keep" async src="https://use.fortawesome.com/349cfdf6.js"></script>
